6a9fc14a1af5600b0b16258dcd5f810b8a2f62d1
Gen <archiherewego@gmail.com>
2017-10-27 21:48:10 +0900

    Update model definition
    how to define indexes on kotlin
    how to define unique on multi column

--- app/src/main/java/jp/gcreate/sample/sampleorma/model/QuestionRelation.kt
+++ app/src/main/java/jp/gcreate/sample/sampleorma/model/QuestionRelation.kt
@@ -1,21 +1,24 @@
 package jp.gcreate.sample.sampleorma.model
 
-import com.github.gfx.android.orma.annotation.*
+import com.github.gfx.android.orma.annotation.Column
+import com.github.gfx.android.orma.annotation.PrimaryKey
+import com.github.gfx.android.orma.annotation.Setter
+import com.github.gfx.android.orma.annotation.Table
 
 /**
  * Copyright 2017 G-CREATE
  */
 @Table(
-    indexes = arrayOf(Index(value = *arrayOf("question", "answer"), unique = true))
+    constraints = arrayOf("unique (question, answer)")
 )
 data class QuestionRelation(
     @PrimaryKey
     @Setter("id")
     val id: Long,
-    @Column
+    @Column(indexed = true)
     @Setter("question")
     val question: Question,
-    @Column
+    @Column(indexed = true)
     @Setter("answer")
     val answer: Answer
 )
